9beaf96f854076f1354e9499f8b0e9827acc2971
Clement83 <clement@quintard.me>
2015-01-25 14:43:41 +0100

    perf
    increase performance

--- src/CrazyTown/AffichageHelper.ino
+++ src/CrazyTown/AffichageHelper.ino
@@ -5,12 +5,14 @@ void drawBitmapAngle(int8_t x, int8_t y, const uint8_t *bitmap,float angle) {
         int8_t centerY = h/2;
 	bitmap = bitmap + 2; //add an offset to the pointer to start after the width and height
     int8_t i, j, byteWidth = (w + 7) / 8;
+    float sinA = sin(angle);
+    float cosA = cos(angle);
     for (j = 0; j < h; j++) {
         for (i = 0; i < w; i++) {
             if (pgm_read_byte(bitmap + j * byteWidth + i / 8) & (B10000000 >> (i % 8))) {
               
-              int8_t desX =   (i - centerX) * cos(angle) - (j-centerY) * sin(angle);
-              int8_t desY = (i-centerX) * sin(angle) + (j-centerY) * cos(angle);
+              int8_t desX =   (i - centerX) * cosA - (j-centerY) * sinA;
+              int8_t desY = (i-centerX) * sinA + (j-centerY) * cosA;
               gb.display.drawPixel(x + desX, y + desY);
             }
         }
